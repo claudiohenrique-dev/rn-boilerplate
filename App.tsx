f6116e389a9c3c35215bf5d33d58262471db2127
claudio-mereo <claudio.andrade@mereo.com>
2022-01-27 23:51:23 -0300

    add babel-plugin-module-resolver

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native'
 
 import { StatusBar } from 'expo-status-bar'
 
-import Button from './src/components/Button'
+import Button from '@components/Button'
 
 export default function App() {
   return (
