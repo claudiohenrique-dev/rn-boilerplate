de4e0dc3241b70db25b562dec260664275d7b6f9
claudio-mereo <claudio.andrade@mereo.com>
2022-01-27 23:36:03 -0300

    add styled-components with TypeScript support

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+
 import { StyleSheet, Text, View } from 'react-native'
+
+import { StatusBar } from 'expo-status-bar'
+
+import Button from './src/components/Button'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style='auto' />
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button />
     </View>
   )
 }
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.TouchableOpacity`
+  background-color: #111312;
+  border-radius: 8px;
+  flex-direction: row;
+  padding: 8px 16px;
+`
+
+export const Text = styled.Text`
+  color: #fafafa;
+  font-size: 16px;
+  font-weight: 700;
+`
